93a8179cf5c1f544db87e9152094cb4b457f3b83
tank <povilasd@neurotechnology.com>
2023-01-23 11:02:42 +0200

    added article draft

--- kb_test.py
+++ kb_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def kb_test(data):
+def kb_test(data, p_value_threshold = 0.05):
     n_samples = int(len(data)/2)
     x = data[:n_samples]
     y = data[n_samples:]
@@ -12,14 +12,20 @@ def kb_test(data):
     result0 = dcor.independence.distance_correlation_t_test(x, y)
     result = dcor.independence.distance_correlation_t_test(x-y,x+y)
 
-    if result.p_value > 0.05:
+    if result.p_value > p_value_threshold:
         print("Gaussian ".format(result.p_value))
     else:
         print("Non-gaussian")    
 
+
+
 n_samples = 500
+p_value_threshold = 0.05
 
 
-kb_test(np.random.normal(0, 1, size=2*n_samples))
-kb_test(np.random.uniform(0, 1, size=2*n_samples))
-kb_test(np.random.laplace(0, 1, size=2*n_samples))
+kb_test(np.random.normal(0, 1, size=2*n_samples), p_value_threshold)
+kb_test(np.random.uniform(0, 1, size=2*n_samples), p_value_threshold)
+kb_test(np.random.laplace(0, 1, size=2*n_samples), p_value_threshold)
+kb_test(np.random.gumbel(0, 1, size=2*n_samples), p_value_threshold)
+kb_test(np.random.lognormal(0, 1, size=2*n_samples), p_value_threshold)
+
