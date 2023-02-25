12e35da65a7bf14c6976d0e01ef5873968f34934
tank <povilasd@neurotechnology.com>
2023-02-25 17:57:57 +0200

    added rezult ploting, updated kb_test

--- kb_test.py
+++ kb_test.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from HSIC import hsic_gam
 from pingouin import multivariate_normality
 from sklearn.metrics import confusion_matrix
+import seaborn
+
 
 class Output():
     def __init__(self):
@@ -22,12 +24,12 @@ def kb_test(data):
     n_iter = 3
 
     for i in range(n_iter):
-        data = np.random.permutation(data)
+        data1 = np.random.permutation(data)
 
         #breakpoint()
 
-        x = data[:n_samples,...]
-        y = data[n_samples:,...]
+        x = data1[:n_samples,...]
+        y = data1[n_samples:,...]
         #result0 = dcor.independence.distance_correlation_t_test(x, y)
         #result = dcor.independence.distance_correlation_t_test(x-y,x+y)
         #testStat, thresh = hsic_gam(x, y, 0.05)
@@ -35,12 +37,12 @@ def kb_test(data):
         delta = testStat1 - thresh1
 
         if delta >= 0:
-                print("Non-gaussian")
+                #print("Non-gaussian")
                 out.normal = False
                 out.delta = delta
                 return out
     
-    print("Gaussian")
+    #print("Gaussian")
     out.normal = True
     out.delta = delta
     return out
@@ -49,84 +51,149 @@ def kb_test(data):
    
 def test(data):
    rez = [] 
-   output0 = kb_test(data) # p_value_threshold = 0.05, n_iter = 2)
+   output0 = kb_test(data) 
    output1 = multivariate_normality(data, alpha=.05)
+   print("G: {} {}".format(output0.normal, output1.normal))
    return output0, output1
 
 
+def monte_carlo_iteration(n_samples = 2000, dim = 100, gaussian_data = True):
+    rez = []
 
-rez = []
-n_samples = 1500
-p_value_threshold = 0.05
-dim = 200
+    A = np.random.rand(dim,dim)
+    cov = np.dot(A, A.transpose())
+    mean = np.zeros(dim)
 
+    #rez.append(test(np.random.multivariate_normal(mu, C, 2*n_samples)))
+    #rez.append(test(np.random.normal(0, 1, size=(2*n_samples, dim))))
+    #
+    # ez.append(test(np.random.uniform(0, 1, size=(2*n_samples, dim))))
+    #rez.append(test(np.random.laplace(0, 1, size=(2*n_samples, dim))))
+    #rez.append(test(np.random.gumbel(0, 1, size=(2*n_samples, dim))))
+    #rez.append(test(np.random.lognormal(0, 1, size=(2*n_samples, dim))))
+    #rez.append(test(np.random.beta(5, 5, size=(2*n_samples, dim))))
 
-rez.append(test(np.random.normal(0, 1, size=(2*n_samples, dim))))
-rez.append(test(np.random.uniform(0, 1, size=(2*n_samples, dim))))
-rez.append(test(np.random.laplace(0, 1, size=(2*n_samples, dim))))
-rez.append(test(np.random.gumbel(0, 1, size=(2*n_samples, dim))))
-rez.append(test(np.random.lognormal(0, 1, size=(2*n_samples, dim))))
-gt = np.zeros(len(rez))
-gt[0] = True
+    if gaussian_data == True:
+        rez.append(test(np.random.multivariate_normal(mean, cov, 2*n_samples)))
+        rez.append(test(np.random.multivariate_normal(np.zeros(dim), np.eye(dim), 2*n_samples)))
+        rez.append(test(np.random.multivariate_normal(2*np.random.uniform(size=dim)-1.0, np.eye(dim), 2*n_samples)))
+        rez.append(test(np.random.multivariate_normal(2*np.random.uniform(size=dim)-1.0, cov, 2*n_samples)))
+        gt = np.ones(len(rez))
 
-stats = []
-
-for x in rez:
-    stats.append([x[0].normal, x[1].normal])
-
-stats = np.array(stats)
-
-cm0 = confusion_matrix(gt, stats[:,0])
-cm1 = confusion_matrix(gt, stats[:,0])
-
-num_gaussian = np.count_nonzero(gt == True)
-num_nongaussian = np.count_nonzero(gt == False)
-
-num_correctly_rejected = 0
-num_correctly_accepted = 0
-
-
-for i in range(len(gt)):
-    if gt[i] == False and stats[i,0] == False:
-        num_correctly_rejected += 1
-
-    if gt[i] == True and stats[i,0] == True:
-        num_correctly_accepted += 1
-
-a = num_correctly_rejected/num_nongaussian
-b = num_correctly_accepted/num_gaussian
-
-print (a)
-print (b)
-num_correctly_rejected = 0
-num_correctly_accepted = 0
+    # wishart
+    if gaussian_data == False:
+        rez.append(test(np.random.chisquare(1, size=(2*n_samples, dim))))
+        rez.append(test(np.random.chisquare(2, size=(2*n_samples, dim))))
+        rez.append(test(np.random.uniform(0, 1, size=(2*n_samples, dim)))) #-1,1
+        rez.append(test(np.random.laplace(0, 1, size=(2*n_samples, dim))))
+        rez.append(test(np.random.logistic(loc=0.0, scale=1.0, size=(2*n_samples, dim)))) #
+        rez.append(test(np.random.logistic(loc=0.0, scale=2.0, size=(2*n_samples, dim)))) #
+        rez.append(test(np.random.power(2, size=(2*n_samples, dim))))
+        rez.append(test(np.random.beta(5,5, size=(2*n_samples, dim)))) #
+        rez.append(test(np.random.beta(8,2, size=(2*n_samples, dim))))
+        rez.append(test(np.random.beta(2,8, size=(2*n_samples, dim))))
+        rez.append(test(np.random.standard_cauchy(size=(2*n_samples, dim))))    
+        #rez.append(test(0.1*np.random.multivariate_normal(np.zeros(dim), C, 2*n_samples) + 0.7*np.random.multivariate_normal(1.0+np.zeros(dim), np.eye(dim,dim), 2*n_samples) ))
+        gt = np.zeros(len(rez))
 
 
-for i in range(len(gt)):
-    if gt[i] == False and stats[i,1] == False:
-        num_correctly_rejected += 1
+    
+    #gt[0] = True
+    #gt[1] = True
+    #gt[2] = True
+    #gt[3] = True
 
-    if gt[i] == True and stats[i,1] == True:
-        num_correctly_accepted += 1
+    stats = []
 
-a = num_correctly_rejected/num_nongaussian
-b = num_correctly_accepted/num_gaussian
+    for x in rez:
+        stats.append([x[0].normal, x[1].normal])
+
+    stats = np.array(stats)
+    #breakpoint()
+
+    #cm0 = confusion_matrix(gt, stats[:,0])
+    #cm1 = confusion_matrix(gt, stats[:,0])
+
+    num_gaussian = np.count_nonzero(gt == True)
+    num_nongaussian = np.count_nonzero(gt == False)
+
+    num_correctly_rejected = 0
+    num_correctly_accepted = 0
+    
+    #print("{} {}".format(num_gaussian, num_nongaussian))
 
 
-print (a)
-print (b)
-#stats[:,0] == True 
-#gt[0] == True
+    for i in range(len(gt)):
+        if gt[i] == False and stats[i,0] == False:
+            num_correctly_rejected += 1
+
+        if gt[i] == True and stats[i,0] == True:
+            num_correctly_accepted += 1
+    
+    p11, p12, p21, p22 = 0.0,0.0,0.0,0.0
+
+    if num_nongaussian > 0:
+        p11 = num_correctly_rejected/num_nongaussian
+
+    if num_gaussian > 0:
+        p12 = num_correctly_accepted/num_gaussian
+
+
+    num_correctly_rejected = 0
+    num_correctly_accepted = 0
+
+
+    for i in range(len(gt)):
+        if gt[i] == False and stats[i,1] == False:
+            num_correctly_rejected += 1
+
+        if gt[i] == True and stats[i,1] == True:
+            num_correctly_accepted += 1
+    
+    if num_nongaussian > 0:
+        p21 = num_correctly_rejected/num_nongaussian
+
+    if num_gaussian > 0:
+        p22 = num_correctly_accepted/num_gaussian
+
+    return np.array([p11,p12,p21,p22])
+
+
+if __name__ == "__main__":
+    num_monte_carlo = 30
+
+    dim = 100
+    num_samples = 2000
+    gaussian_data = True
+    dims = []
+    rezults = []
+    for dim in range(50, 160, 10):
+        print('Analysing dimension {}'.format(dim))
+        dims.append(dim)
+        rez = []
+        for i in range(num_monte_carlo):
+            rez.append(monte_carlo_iteration(num_samples,dim, gaussian_data))
+            print("---")
+        rez = np.array(rez)
+        if gaussian_data == True:
+            mean_rez = np.mean(rez,axis=0)[[1,3]]
+            std_rez = np.std(rez,axis=0)[[1,3]]
+        else:
+            mean_rez = np.mean(rez,axis=0)[[0,2]]
+            std_rez = np.std(rez,axis=0)[[0,2]]
+
+        rezults.append(rez)
+        print(mean_rez)
+
+    np.save('./dims_normal.npy', dims)
+    np.save('./rezults_normal.npy', rezults)
+    """
+    fig1, ax1 = plt.subplots()
+    ax1.set_title('Basic Plot')
+    ax1.boxplot(rez, showfliers=False)
+    #seaborn.violinplot(rez)
+    plt.show()
+    """
+
 breakpoint()  
-
-#o = kb_test(np.random.normal(0, 1, size=(2*n_samples, dim)), p_value_threshold)
-#o = kb_test(np.random.uniform(0, 1, size=(2*n_samples, dim)), p_value_threshold)
-#o = kb_test(np.random.laplace(0, 1, size=(2*n_samples, dim)), p_value_threshold)
-#o = kb_test(np.random.gumbel(0, 1, size=(2*n_samples, dim)), p_value_threshold)
-#o = kb_test(np.random.lognormal(0, 1, size=(2*n_samples, dim)),p_value_threshold)
-
-
-#mean = [100, 0]
-#cov = [[1, 0], [0, 1]]  # diagonal covariance
-#val, stat, th = kb_test(np.random.multivariate_normal(mean, cov, size=2*n_samples), p_value_threshold)
 
---
+++ plot_rezults.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+dims = np.load('./dims_normal.npy')
+rezults = np.load('./rezults_normal.npy')
+
+
+
+rezults_kb = []
+rezults_hz = []
+stds_kb = []
+stds_hz = []
+for d,rez in zip(dims, rezults):
+    #print("{} {}".format(d, np.mean(rez, axis=0)))
+    mn = np.mean(rez, axis=0)    
+    std = np.std(rez, axis=0)
+    rezults_kb.append(mn[1])
+    rezults_hz.append(mn[3])
+    stds_kb.append(std[1])
+    stds_hz.append(std[3])
+    #print("{} {} {}".format(d, mn[0], mn[2]))
+
+dims = np.load('./dims.npy')
+rezults = np.load('./rezults.npy')
+rezults_kb1 = []
+rezults_hz1 = []
+stds_kb1 = []
+stds_hz1 = []
+for d,rez in zip(dims, rezults):
+    #print("{} {}".format(d, np.mean(rez, axis=0)))
+    mn = np.mean(rez, axis=0)    
+    std = np.std(rez, axis=0)
+    rezults_kb1.append(mn[0])
+    rezults_hz1.append(mn[2])
+    stds_kb1.append(std[0])
+    stds_hz1.append(std[2])
+    #print("{} {} {}".format(d, mn[0], mn[2]))
+plt.subplot(1, 2, 1)
+plt.errorbar(dims, rezults_kb, stds_kb) #, 'b--')
+plt.errorbar(dims, rezults_hz, stds_hz) #, 'r--')
+plt.subplot(1, 2, 2)
+plt.errorbar(dims, rezults_kb1, stds_kb1) #, 'b--')
+plt.errorbar(dims, rezults_hz1, stds_hz1) #, 'r--')
+plt.show()
+
+#breakpoint()    
